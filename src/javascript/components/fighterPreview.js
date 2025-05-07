b6724f78cb2be9c7c07327b4b7adb0d141ffefad
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2025-05-07 13:06:42 +0300

    ighterPreview

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,46 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    if (!fighter) return fighterElement;
+    const fighterImg = createFighterImage(fighter);
+    const infoContainer = createElement({
+        tagName: 'ul',
+        className: `fighter-preview___infoContainer`
+    });
+    const fighterName = createElement({
+        tagName: 'li',
+        className: `fighter-preview___infoItem`
+    });
+    fighterName.innerText = `Name: ${fighter.name}`;
+
+    const fighterHealth = createElement({
+        tagName: 'li',
+        className: `fighter-preview___infoItem`
+    });
+    fighterHealth.innerText = `Health: ${fighter.health}`;
+
+    const fighterAttack = createElement({
+        tagName: 'li',
+        className: `fighter-preview___infoItem`
+    });
+    fighterAttack.innerText = `Attack: ${fighter.attack}`;
+
+    const fighterDefense = createElement({
+        tagName: 'li',
+        className: `fighter-preview___infoItem`
+    });
+    fighterDefense.innerText = `Defense: ${fighter.defense}`;
+
+    infoContainer.append(fighterName, fighterHealth, fighterAttack, fighterDefense);
+    fighterElement.append(fighterImg, infoContainer);
+    return fighterElement;
 }
